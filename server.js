820dd94dd7fafb98e7a9c825e75a6d006a98c3f4
lean270804@gmail.com <lean270804@gmail.com>
2025-10-25 23:43:18 +0700

    Add API Fetch All Order

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.use(express.json());
 app.use(cors({
   origin: process.env.FRONTEND_URL, 
   credentials: true,
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
 }));
 
 app.use(cookieParser())
